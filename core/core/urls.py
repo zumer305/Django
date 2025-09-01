f94070b7b2c0ba634adff77ec93cfa33e01864f6
zumer305 <zumerniaz305@gmail.com>
2025-09-01 15:53:53 +0700

    message print

--- core/core/urls.py
+++ core/core/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from home.views import *
+
+
 urlpatterns = [
+
+  path('', home),
     path('admin/', admin.site.urls),
 ]
