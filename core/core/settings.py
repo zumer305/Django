9a9cdf79701f235e5a1b1ea07586da15a7735508
zumer305 <zumerniaz305@gmail.com>
2025-09-01 15:24:54 +0700

    update apps files

--- core/core/settings.py
+++ core/core/settings.py
@@ -38,7 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+EXTERNAL_APPS=[
+    'accounts',
+    'home',
+]
 
+INSTALLED_APPS += EXTERNAL_APPS
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
