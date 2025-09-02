985a5437e3e289b0374871bb5dde1d670f060ee8
zumer305 <zumerniaz305@gmail.com>
2025-09-02 13:17:53 +0500

    update

--- core/home/views.py
+++ core/home/views.py
@@ -5,10 +5,11 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home (request):
-    return HttpResponse("""<h1>Hey I am Django sserver.<h1>
-                        <p> I am Django sserver.<p>
-                        <hr>
-                        <h1>Hey I am Django sserver.<h1>""")
+    # return HttpResponse("""<h1>Hey I am Django server.<h1>
+    #                     <p> I am Django server.<p>
+    #                     <hr>
+    #                     <h2 style="color:red" >Hey I am Django sserver.<h2>""")
+    return render(request,"index.html")
 
 
 def success (request):
